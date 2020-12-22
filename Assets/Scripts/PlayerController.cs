45278f81ac3ca77dff757fa534d4f57d225ec62d
Nakagawakeita <nakagawakeita10@gmail.com>
2020-12-22 09:32:24 +0900

    手順７終了

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,6 +25,13 @@ public class PlayerController : MonoBehaviour
     //ジャンプ・浮遊力
     public float jumpPower;
 
+    //地面の元となるものをアサインする為の変数
+    [SerializeField,Header("Linecast用地面判定レイヤー")]
+    private LayerMask groundLayer;
+
+    //地面に設置しているかどうかを判定するための変数
+    public bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,11 +46,27 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        //地面接地Phisics2D.Linecastメソッドを実行して、Ground Layerとキャラのコライダーとが
+        //接地しているか確認し、接地しているならtrue、していないならfalseを戻す
+        isGrounded = Physics2D.Linecast(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, groundLayer);
+
+        //SceneビューにPhisics2D.LinecastメソッドのLineを表示する
+        Debug.DrawLine(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, Color.red, 1.0f);
+
         //ジャンプ
         //InputManagerのjumpの項目に登録されているキー入力を判定する
+        //jumpボタン(space)を押した場合
         if (Input.GetButtonDown(jump))
         {
+            //Jumpメソッドを実行
             Jump();
+        }
+
+        //接地していない(空中にいる)間で、落下中の場合
+        if (isGrounded == false && rb.velocity.y < 0.15f)
+        {
+            //落下アニメを繰り返す
+            anim.SetTrigger("Fall");
         }
     }
 
@@ -53,9 +76,11 @@ public class PlayerController : MonoBehaviour
     private void Jump()
     {
         //キャラの位置を上方向へ移動させる(ジャンプ・浮遊)
+        //Rigidbody2DのAddForce関数でy方向*jumpPower移動させる
         rb.AddForce(transform.up * jumpPower);
 
         //jump(Up + Mid)アニメーションを再生する
+        //AnimatorのTriggerのJumpとあるアニメーションを再生する
         anim.SetTrigger("Jump");
     }
 
